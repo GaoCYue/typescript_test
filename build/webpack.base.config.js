28f090d901bcd19ef6d34094e39a1ae853cc66f3
gao <1536576466@qq.com>
2020-08-07 17:45:07 +0800

    调整webpack

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,10 +1,11 @@
 // 引入插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const path = require('path')
 module.exports = {
   entry: './src/index.ts', // 入口文件
   output: {
-    filename: 'app.js' // 打包文件名，默认在 `dist` 目录下
+    filename: 'app.js', // 打包文件名，默认在 `dist` 目录下
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
@@ -23,6 +24,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      // favicon: '../public/favicon.ico',
+      inject: 'body', //js插入的位置，true/'head'/'body'/false
+      minify: { //压缩HTML文件
+          removeComments: true, //移除HTML中的注释
+          collapseWhitespace: false //删除空白符与换行符
+      },
       template: './public/index.html' // 模板 html
     }),
     // new ForkTsCheckerWebpackPlugin()
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const app = document.getElementById('app')
+const appStyle = window.getComputedStyle(app as Element)
+console.log(appStyle)
